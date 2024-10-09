bb9e3d411d2f6274b121ca411979f1e52d80b51d
n-pontes <nuno06.np@gmail.com>
2024-10-09 18:01:39 +0200

    Simplify arrow functions for better readability

--- script/calculator.js
+++ script/calculator.js
@@ -1,43 +1,24 @@
 // Operator variable
-
 let operator = ['+', '-', '/', '*'];
 
 // Operator numbers
-
 let num1 = 0;
 let num2 = 0;
 
 // Addition function
-
-const add = ((a, b) => {
-    return a + b;
-});
-
+const add = (num1, num2) => num1 + num2;
 console.log("The result is:", add(1, 6));
 
 // Subtraction function
-
-const sub = ((a, b) => {
-    return a - b;
-});
-
+const sub = (num1, num2) => num1 - num2;
 console.log("The result is:", sub(8, 4));
 
 // Multiply function
-
-const multiply = ((numb1, num2) => {
-    let result = numb1 * num2;
-    return result;
-});
-
+const multiply = (numb1, num2) => numb1 * num2;
 console.log("The result is:", multiply(4, 4));
 
 // Division function
-
-const divide = ((a, b) => {
-    return a / b;
-});
-
+const divide = (num1, num2) => num1 / num2;
 console.log("The result is:", divide(50, 5));
 
 const operate = ((operator, num1, num2) => {
