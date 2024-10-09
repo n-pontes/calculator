2039881febd1e05b9c7b089ded46a99f5235470a
n-pontes <nuno06.np@gmail.com>
2024-10-09 16:24:21 +0200

    Add to comments to functions

--- script/calculator.js
+++ script/calculator.js
@@ -14,12 +14,16 @@ const sub = ((a, b) => {
 
 console.log("The result is:", sub(8, 4));
 
+// Multiply function
+
 const multiply = ((numb1, num2) => {
     let result = numb1 * num2;
     return result;
 });
 
 console.log("The result is:", multiply(4, 4));
+
+// Division function
 
 const divide = ((a, b) => {
     return a / b;
