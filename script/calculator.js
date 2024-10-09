62075a5bb73e56b76687ad65a5c8fe67e24851e9
n-pontes <nuno06.np@gmail.com>
2024-10-09 16:19:39 +0200

    Add multiplication function
    - Given two arguments (num1, num2) the function performs an arithmetic operation of multiplication
    - The function passes those two arguments and returns them

--- script/calculator.js
+++ script/calculator.js
@@ -13,3 +13,10 @@ const sub = ((a, b) => {
 });
 
 console.log("The result is:", sub(8, 4));
+
+const multiply = ((numb1, num2) => {
+    let result = numb1 * num2;
+    return result;
+});
+
+console.log("The result is:", multiply(4, 4));
